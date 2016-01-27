8725d63c495c96100ebf34a081b655013e09718c
Zixian YANG <532705376@qq.com>
2016-01-27 13:57:11 +0800

    Two bears are running
    Two bears are running

---
+++ src/app.js
@@ -0,0 +1,34 @@
+
+var HelloWorldLayer = cc.Layer.extend({
+    sprite:null,
+    ctor:function () {
+        //////////////////////////////
+        // 1. super init first
+        this._super();
+
+        /////////////////////////////
+        // 2. add a menu item with "X" image, which is clicked to quit the program
+        //    you may modify it.
+        // ask the window size
+        var size = cc.winSize;
+
+        var mainscene = ccs.load(res.MainScene_json);
+        var node = mainscene.node;
+        var action = mainscene.action;
+        node.runAction(action);
+        action.gotoFrameAndPlay(0,25,0,true);
+        node.runAction(action);
+        this.addChild(node);
+
+        return true;
+    }
+});
+
+var HelloWorldScene = cc.Scene.extend({
+    onEnter:function () {
+        this._super();
+        var layer = new HelloWorldLayer();
+        this.addChild(layer);
+    }
+});
+
