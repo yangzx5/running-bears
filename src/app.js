6558a4df19e910034bac61db778945f80e4be6f2
Zixian YANG <532705376@qq.com>
2016-02-06 00:24:29 +0800

    The bear is running on the grass straightly

--- src/app.js
+++ src/app.js
@@ -10,15 +10,37 @@ var HelloWorldLayer = cc.Layer.extend({
         // 2. add a menu item with "X" image, which is clicked to quit the program
         //    you may modify it.
         // ask the window size
-        var size = cc.winSize;
+        var size = cc.director.getWinSize();
+        //var bear1_2 = ccs.load(res.Bear1_2_json);
+        //var node = bear1_2.node;
+        //var action = bear1_2.action;
+        //node.runAction(action);
+        //action.gotoFrameAndPlay(0,25,0,true);
+        //node.runAction(action);
+        //this.addChild(node);
+        var bg = new cc.Sprite(res.Background);
+        bg.x = size.width/2;
+        bg.y = size.height/2;
+        this.addChild(bg);
 
-        var mainscene = ccs.load(res.MainScene_json);
-        var node = mainscene.node;
-        var action = mainscene.action;
-        node.runAction(action);
-        action.gotoFrameAndPlay(0,25,0,true);
-        node.runAction(action);
-        this.addChild(node);
+        var sprite = new cc.Sprite(res.Bear1_2_png);
+        sprite.x = size.width/2;
+        sprite.y = size.height/2;
+        this.addChild(sprite);
+
+        var animation = new cc.Animation();
+        for(var i=0;i<6;i++)
+        {
+            var spriteFrame = new cc.SpriteFrame(g_resources[i],cc.rect(0,0,573,504));
+            animation.addSpriteFrame(spriteFrame);
+        }
+        animation.setDelayPerUnit(0.15);
+        animation.setRestoreOriginalFrame(true);
+        var action = cc.animate(animation);
+        var action1 = cc.repeat(action,4);
+        var action2 = cc.moveBy(3.6,cc.p(500,0));
+        var action3 = cc.spawn(action1,action2);
+        sprite.runAction(action3);
 
         return true;
     }
--- src/resource.js
+++ src/resource.js
@@ -1,5 +1,18 @@
 var res = {
-    MainScene_json : "res/MainScene.json"
+    Bear1_2_png : "res/bear1/2.png",
+    Bear1_3_png : "res/bear1/3.png",
+    Bear1_4_png : "res/bear1/4.png",
+    Bear1_5_png : "res/bear1/5.png",
+    Bear1_6_png : "res/bear1/6.png",
+    Bear1_7_png : "res/bear1/7.png",
+    Bear2_2_png : "res/bear2/2.png",
+    Bear2_3_png : "res/bear2/3.png",
+    Bear2_4_png : "res/bear2/4.png",
+    Bear2_5_png : "res/bear2/5.png",
+    Bear2_6_png : "res/bear2/6.png",
+    Bear2_7_png : "res/bear2/7.png",
+    Net_png : "res/net.png",
+    Background : "res/background.png"
 };
 
 var g_resources = [];
